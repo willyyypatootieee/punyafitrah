6be76b1ce153f0941b3910f52b3314b8266d1f79
willyyypatootieee <blueplayz26@gmail.com>
2024-10-14 20:32:08 +0700

    javascript kontol

--- script.js
+++ script.js
@@ -1,33 +1,35 @@
-        // ganti color
-        const bgColorSelector = document.getElementById('bg-color');
-        bgColorSelector.addEventListener('change', function() {
-            document.body.style.backgroundColor = this.value;
-        });
+const bgColorSelector = document.getElementById('bg-color');
+bgColorSelector.addEventListener('change', function() {
+    document.body.style.backgroundColor = this.value;
 
-        // text size
-        const fontSizeSlider = document.getElementById('font-size');
-        fontSizeSlider.addEventListener('input', function() {
-        document.documentElement.style.fontSize = this.value + 'px'; // Change root font-size
-        });
+    if (this.value !== '#f4f4f4' && this.value !== '#ffcccb' && this.value !== '#cceeff' && this.value !== '#ccffcc') {
+        document.body.classList.remove('dark-mode');
+    }
+});
 
+const fontSizeSlider = document.getElementById('font-size');
+fontSizeSlider.addEventListener('input', function() {
+    document.documentElement.style.fontSize = this.value + 'px'; // Change root font-size
+});
 
-        // tombol dark mode
-        const darkModeToggle = document.getElementById('dark-mode-toggle');
-        darkModeToggle.addEventListener('click', function() {
-            document.body.classList.toggle('dark-mode');
-        });
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+darkModeToggle.addEventListener('click', function() {
+    document.body.classList.toggle('dark-mode');
+});
 
-        // ganti font
-        const serifFontButton = document.getElementById('serif-font');
-        const sansSerifFontButton = document.getElementById('sans-serif-font');
-        const container = document.getElementById('mainContainer');
+// ganti font
+const serifFontButton = document.getElementById('serif-font');
+const sansSerifFontButton = document.getElementById('sans-serif-font');
+const container = document.getElementById('mainContainer');
 
-        serifFontButton.addEventListener('click', function() {
-            container.classList.remove('sans-serif');
-            container.classList.add('serif');
-        });
+// inisialisasi font bebas 
+document.body.style.fontFamily = "'Bebas Neue', sans-serif";
 
-        sansSerifFontButton.addEventListener('click', function() {
-            container.classList.remove('serif');
-            container.classList.add('sans-serif');
-        });
+serifFontButton.addEventListener('click', function() {
+
+    document.body.style.fontFamily = "serif";
+});
+
+sansSerifFontButton.addEventListener('click', function() {
+    document.body.style.fontFamily = "'Bebas Neue', sans-serif";
+});
